7ec9df96b09a952afbab7aab0495a193c767c5c3
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-06-02 23:08:22 +0300

    Solution of the task 39 (create new array, записав цифры с конца; отредактировал имеющийся массив путем замены значений с помощью новой переменной)

---
+++ task_39_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// Например:
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+// Решение данной задачи путём замены значений в обном массиве
+
+int[] array = CreateArray(9, 1, 10);
+Console.Write($"[{String.Join(", ", array)}] --> "); ReversArray(array);
+
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void ReversArray(int[] array)
+{
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        int tmp = array[i];
+        array[i] = array[array.Length - 1 - i];
+        array[array.Length - 1 - i] = tmp;
+    }
+    Console.Write($"[{String.Join(", ", array)}]");
+}
---
+++ task_39_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// Например:
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+// Решение данной задачи путём создания нового массива переписанием начального с конца
+
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void ReverseArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - 1 - i];
+    }
+    Console.Write($"[{String.Join(", ", result)}]");
+}
+
+int[] array = CreateArray(9, 1, 10);
+Console.Write($"[{String.Join(", ", array)}] --> "); ReverseArray(array);
