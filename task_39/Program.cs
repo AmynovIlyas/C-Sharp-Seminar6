29c1c0e4e96e8c3e82fd323689a27d45707f7f4b
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-06-02 21:57:48 +0300

    Solution of the task 39 (create new array from half first array)

---
+++ task_39/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, 
+// а первый - на последнем и т.д.)
+// Например:
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+// Решение данной задачи путём создания нового массива, обработав половину массива
+
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+void UpheavalArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    if (array.Length % 2 == 0)
+    {
+        for (int i = 0; i < array.Length / 2; i++)
+        {
+            result[i] = array[array.Length - 1 - i];
+            result[array.Length - 1 - i] = array[i];
+        }
+    }
+    else
+    {
+        for(int i = 0; i < array.Length / 2 + 1; i++)
+        {
+            result[i] = array[array.Length - 1 - i];
+            result[array.Length - 1 - i] = array[i];
+        }
+    }
+    Console.Write($"[{String.Join(", ", result)}]");
+}
+
+int[] array = CreateArray(9, 0, 10);
+Console.Write($"[{String.Join(", ", array)}] --> "); UpheavalArray(array);
+
