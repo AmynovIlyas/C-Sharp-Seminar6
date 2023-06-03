7116149ab0d3f4f07ecba16e2cd53b1c522ec7de
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-06-03 15:11:36 +0300

    Solution of the task 42 (путём выведения остатков от деления на 2 в строку с дальнейшим ее переворотом(либо без переворота)

---
+++ task_42_1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Например: 
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+// Решение данной задачи путем выведения остатка от деления на два в виде строки и последующий переворот этой строки
+
+
+Console.WriteLine("Введите число для преобразования: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number} --> {ToBinary(number)}");
+
+string ReverseString(string str)
+{
+    string result = String.Empty;
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+        result += str[i];
+    }
+    return result;
+}
+
+string ToBinary(int num)
+{
+    string result = String.Empty;
+    while(num > 0)
+    {
+        result += num % 2;
+        num /= 2;
+    }
+    return ReverseString(result);
+}
+
---
+++ task_42_Magiya/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Например: 
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+// Решение данной задачи путем выведения остатка от деления на два в строку с использованием способа,
+// освобождающего от необходимости переворачивать строку
+
+
+Console.WriteLine("Введите число для преобразования: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number} --> {ToBinary(number)}");
+
+
+string ToBinary(int num)
+{
+    string result = String.Empty;
+    while(num > 0)
+    {
+        result = num % 2 + result; // заменяем действие "result += num % 2" на "result = num % 2 + result"
+        num /= 2;
+    }
+    return result;
+}
+
+
