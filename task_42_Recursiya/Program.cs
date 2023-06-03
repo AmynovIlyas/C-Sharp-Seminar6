83d58b0650daea1cecebfcd4460acbe2fb99ee8c
Ilyas Aminov <aminov.i.g@yandex.ru>
2023-06-03 22:49:28 +0300

    Solution of the task 42 (неудачная попытка решения через рекурсию)

---
+++ task_42_Recursiya/Program.cs
@@ -0,0 +1,23 @@
+﻿// // Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// Например: 
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+// Решение данной задачи путем использования метода рекурсии
+
+Console.WriteLine("Введите число для преобразования: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{number} --> {ToBinary(number)}");
+
+string ToBinary(int num)
+{
+    string result = String.Empty;
+    while(num > 0)
+    {
+        result += (num % 2).ToString();
+        num /= 2;
+    }
+    return ToBinary(result); 
+}
+
+
